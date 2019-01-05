0766ce52e9800564e8c02dabc83cb94c48f9470e
Alina <alina.alexandrovna.sh@gmail.com>
2019-01-06 01:51:28 +0300

    add db connection

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,60 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+// import resources.driver;
+
 
 public class Main {
     
+    private static final String url = "jdbc:mysql://localhost:3306/test";
+    private static final String user = "root";
+    private static final String password = "1";
+
+    private static Connection connection = null;
+    private static Statement statement = null;
+    private static ResultSet resSet = null;
+
     public static void main(final String... args)
     throws Exception {
-        System.out.println("JDBC Bench");
+
+        String queryUpdate = "INSERT INTO table_1 VALUES ('1111111111', 'kjfhsdkj')";
+        String querySelect = "SELECT * FROM table_1";
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "1");
+            statement = connection.createStatement();
+            resSet = statement.executeQuery(querySelect);
+
+            while (resSet.next()) {
+                String key = resSet.getString(1);
+                String value = resSet.getString(2);
+                System.out.println("selected: " + key + " " + value);
+            }
+
+            // statement.executeUpdate(query);
+
+            connection.close();
+            statement.close();
+            resSet.close();
+
+        } catch (SQLException sqlEx) {
+            sqlEx.printStackTrace();
+        } catch (Exception exc) {
+            exc.printStackTrace();
+            System.err.print("Something went wrong!");
+        } finally {
+            // try { connection.close(); } catch ( SQLException exc) { }
+            // try { statement.close(); } catch ( SQLException exc) { }
+            // try { resSet.close(); } catch ( SQLException exc) { }
+        }
     }
+
+    // private void connectDB() {
+
+    // }
 }
