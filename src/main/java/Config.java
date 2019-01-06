649bea934ef86abfe9558bb45f4c03103b14dd47
Alina <alina.alexandrovna.sh@gmail.com>
2019-01-06 17:30:03 +0300

    added gradle

---
+++ src/main/java/Config.java
@@ -0,0 +1,16 @@
+
+public class Config {
+    private String dbHost;
+    private String dbPort;
+    private String dbName;
+    private String accessCredentialUsername;
+    private String accessCredentialPassword;
+    private String tableName;
+    private String outputFileName;
+    private Integer payloadSize;
+    private Integer countInsertOperations;
+    private Integer countInsertionThreads;
+
+    
+
+}
