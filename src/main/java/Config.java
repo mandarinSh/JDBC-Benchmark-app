81e924c8935eb3884106ef03530421bd6d692422
Alina <alina.alexandrovna.sh@gmail.com>
2019-01-07 23:30:29 +0300

    some changes in build.gradle

--- src/main/java/Config.java
+++ src/main/java/Config.java
@@ -1,4 +1,11 @@
-import org.apache.commons.cli.*;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Option;
+
 
 
 public class Config {
